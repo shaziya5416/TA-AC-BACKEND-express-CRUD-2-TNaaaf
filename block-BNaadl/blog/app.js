fb35876e12dcf4e419655e71928bfc6f5ddc7cb4
Shazia Khan <shaziya5416@gmail.com>
2022-03-23 13:31:10 +0530

    changes

--- block-BNaadl/blog/app.js
+++ block-BNaadl/blog/app.js
@@ -5,7 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var articlesRouter = require('./routes/articles');
+const { default: mongoose } = require('mongoose');
+
+//connect to database
+mongoose.connect("mongodb://localhost/blog",(err)=>{
+console.log(err?err:"Connected to DataBase");
+}
+);
 
 var app = express();
 
@@ -20,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/articles', articlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
